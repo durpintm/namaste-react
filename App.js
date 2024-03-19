311e0c5c6caee956da064c98eaf1dfa5729be6d8
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-19 19:54:43 -0400

    Adding parcel and react packages with updated notes and readme

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World! from React"
-// );
-
-// ReactElement(Object) => HTML(Browser Understands)
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am a H1 tag"),
+    React.createElement("h1", {}, "This is Namaste React 🚀"),
     React.createElement("h2", {}, "I am a H2 tag"),
   ]),
   React.createElement("div", { id: "child" }, [
