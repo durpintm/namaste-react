4ecc39b6277c9c06e138db4ec4e0ddb299f01151
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-24 17:23:31 -0400

    Component Composition

--- App.js
+++ App.js
@@ -5,14 +5,25 @@ const heading = <h1 className="heading">Namaste React using JSX 😎</h1>;
 
 const fn = () => <h1>Hello Function</h1>;
 
-// Both HeadingComponent and HeadingComponent2 are React Functional Component
-const HeadingComponent = () => {
-  return <h1>Namaste React from Functional Component</h1>;
+const Title = () => {
+  return (
+    <h1 className="head" tabIndex="5">
+      Namaste React using JSX 🚀
+    </h1>
+  );
 };
 
-const HeadingComponent2 = () => (
-  <h1>Namaste React from Functional Component</h1>
+// Both HeadingComponent and HeadingComponent2 are React Functional Component
+const HeadingComponent = () => (
+  <div className="container">
+    <Title />
+    <h1>Namaste React from Functional Component</h1>
+  </div>
 );
 
+// const HeadingComponent2 = () => (
+//   <h1>Namaste React from Functional Component</h1>
+// );
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
