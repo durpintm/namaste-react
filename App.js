260da2ff48d01bc79cd77d26456c3697722e468a
Durpin Thapa Magar <durpinthapa@gmail.com>
2024-03-24 17:43:38 -0400

    Adding js code and html elements inside component

--- App.js
+++ App.js
@@ -5,25 +5,40 @@ const heading = <h1 className="heading">Namaste React using JSX 😎</h1>;
 
 const fn = () => <h1>Hello Function</h1>;
 
-const Title = () => {
-  return (
-    <h1 className="head" tabIndex="5">
-      Namaste React using JSX 🚀
-    </h1>
-  );
-};
+// Functional Component
+// const Title = () => {
+//   return (
+//     <h1 className="head" tabIndex="5">
+//       Namaste React using JSX 🚀
+//     </h1>
+//   );
+// };
+
+const elm = <span>This is a react element</span>;
+
+const title = (
+  <h1 className="head" tabIndex="5">
+    {elm}
+    Namaste React using JSX 🚀
+  </h1>
+);
+
+const number = 10000;
 
 // Both HeadingComponent and HeadingComponent2 are React Functional Component
+// Component Composition => Component inside a component
 const HeadingComponent = () => (
   <div className="container">
-    <Title />
+    {/* <h2>{number}</h2> */}
+    {/* <Title /> */}
+    {title}
     <h1>Namaste React from Functional Component</h1>
   </div>
 );
 
-// const HeadingComponent2 = () => (
-//   <h1>Namaste React from Functional Component</h1>
-// );
+// const HeadingComponent2 = () => {
+//   return <h1>Namaste React from Functional Component</h1>;
+// };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HeadingComponent />);
